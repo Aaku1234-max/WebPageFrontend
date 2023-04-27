326aeba460fad1b746779488300c43e84adab060
Aakanksha Raosaheb barkale <89270818+Aaku1234-max@users.noreply.github.com>
2023-04-27 12:37:09 +0530

    Add files via upload

---
+++ Day3/day3.js
@@ -0,0 +1,47 @@
+alert("congratulation")
+alert("Hello")
+/*alert(4+2)
+let num=15
+alert(num)
+
+var num1=20
+alert(num)
+
+const num2=7
+alert(num2)
+num2=22
+alert(num2)*/
+var a=2
+var b=5
+/*sum=a*b
+alert(sum)*/
+alert(a*=b)
+
+let x=6,y=5
+alert(x+=y)
+
+/*assignment operater
+let a=13
+a*=5
+alert(a)
+
+//relational
+var res=12==0
+alert(res)*/
+
+//logical 
+var ab=20,cb=12
+alert(ab==cb && ab>cb)
+if(ab<cb)
+    alert("Aakanksha")
+    else
+    alert("Barkale")
+    //typeof
+    //alert(typeof true)//boolean
+    alert(typeof "Aakanksha")//string
+    alert(typeof 33.2)
+
+    let age=prompt("what is your age?",21)
+    alert("your age is"+age)
+
+
---
+++ Day3/intro.js
@@ -0,0 +1,6 @@
+function printlength(){
+    var str=document.getElementById("name").value;
+    var strlen=str.length;
+    const x=44;
+    document.getElementById("demo").innerHTML="your score is:"+strlen+x;
+}
